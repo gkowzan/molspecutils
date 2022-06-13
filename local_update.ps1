50b8b2d0b96d12af4689e657388332617adf6000
Grzegorz Kowzan <grzegorz@kowzan.eu>
2022-06-13 16:32:46 -0400

    add scripts

---
+++ local_update.ps1
@@ -0,0 +1,12 @@
+python -m build
+if ( $LASTEXITCODE -ne 0 ) {
+    Write-Error -Message "Build failed" -ErrorAction Stop
+}
+
+twine upload -r local dist/*
+
+cd ../rotsim2d
+pip install --extra-index-url=http://127.0.0.1:4040/simple --no-deps --pre --upgrade --force-reinstall --no-cache-dir molspecutils
+cd ../rotsim2d_apps
+pip install --extra-index-url=http://127.0.0.1:4040/simple --no-deps --pre --upgrade --force-reinstall --no-cache-dir molspecutils
+cd ../molspecutils
